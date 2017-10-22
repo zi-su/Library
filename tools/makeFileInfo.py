17acf339893b4a23e2c8bcdc7e9a21a2ab857c80
Masato Naito <aitona765@gmail.com>
2017-10-22 20:25:14 +0900

    エンディアン吸収、ctypes使用

--- tools/makeFileInfo.py
+++ tools/makeFileInfo.py
@@ -5,6 +5,21 @@ import glob
 import hashlib
 import struct
 import re
+import ctypes
+
+class FileInfoBigEndian(ctypes.BigEndianStructure):
+    _fields_ = (
+        ('filePath', ctypes.c_char * 512),
+        ('fileSize', ctypes.c_uint32),
+        ('hashValue', ctypes.c_char * 32),
+    )
+
+class FileInfoLittleEndian(ctypes.LittleEndianStructure):
+    _fields_ = (
+        ('filePath', ctypes.c_char * 512),
+        ('fileSize', ctypes.c_uint32),
+        ('hashValue', ctypes.c_char * 32),
+    )
 
 basePath = os.path.join(os.path.abspath(__file__), '../')
 refpath = os.path.normpath(os.path.join(basePath, '../resource'))
@@ -23,26 +38,31 @@ fb = open(outbinPath, 'wb')
 barray = bytearray()
 f.write('{\n')
 filelist = GetFileList(resourcePath)
+fileInfoList = list()
 
 def MakeFileInfo(endian):
     for i in filelist:
         i = i.replace('\\','/')
-        filesize = os.path.getsize(i)
+        fileSize = os.path.getsize(i)
         filename = os.path.basename(i)
         hashValue = hashlib.md5(filename.encode('utf-8')).hexdigest()
-        filesizebytes = filesize.to_bytes(4,endian)
         index = i.find('resource')
         filePath = i[index:len(i)]
+        fileSizeBytes = fileSize.to_bytes(4, endian)
         print(i)
-        print(filesize)
-        print(filesizebytes)
-        f.write('\t["filePath":"{0}", "fileSize":{1}, "hash":{2}],\n'.format(filePath, filesize, hashValue))
-        barray.extend(filePath.encode('utf-8'))
-        barray.extend(filesizebytes)
-        barray.extend(hashValue.encode('utf-8'))
+        print(fileSize)
+        print(fileSizeBytes)        
+        f.write('\t["filePath":"{0}", "fileSize":{1}, "hash":{2}],\n'.format(filePath, fileSize, hashValue))
+        if endian == 'little':
+            fileinfo = FileInfoLittleEndian(filePath.encode('utf-8'), fileSize, hashValue.encode('utf-8'))
+        else:
+            fileinfo = FileInfoBigEndian(filePath.encode('utf-8'), fileSize, hashValue.encode('utf-8'))
+        
+        fileInfoList.append(fileinfo)
     f.write('}')
     f.close()
-    fb.write(barray)
+    for i in fileInfoList:
+        fb.write(i)
     fb.close()
 
 MakeFileInfo('little')
